f192bb4b4eff74a59a8445cd2b988058cac644ed
Erick <erickpaludoyt@gmail.com>
2025-02-03 13:38:00 -0300

    1.0.1 - Site Cadastro - Iniciada lógica do cadastro

--- SiteCadastro/js/script.js
+++ SiteCadastro/js/script.js
@@ -1,23 +1,31 @@
-$(function () {
+function Cadastrar() {
+    var usuario = {
+        firstName: document.getElementById("nome").value.trim(),
+        lastName: document.getElementById("sobrenome").value.trim(),
+        userName: document.getElementById("usuario").value.trim(),
+        userPass: document.getElementById("senha").value.trim(),
+        email: document.getElementById("email").value.trim()
+    };
 
-
-    //HEADER
-    $(window).scroll(function () {
-          if($(this).scrollTop() > 200)
-          {
-              if (!$('.main_header').hasClass('fixed'))
-              {
-                  $('.main_header').stop().addClass('fixed').css('top', '-100px').animate(
-                      {
-                          'top': '0px'
-                      }, 500);
-              }
-          }
-          else
-          {
-              $('.main_header').removeClass('fixed');
-          }
-    });
-
-
-});
+    if (usuario.firstName === "" || usuario.lastName === "" || usuario.userName === "" || usuario.userPass === "" || usuario.email === "") {
+        console.log("Objeto Usuario:", usuario);
+        alert("Todos os campos devem ser preenchidos!");
+        return;
+    }
+    else{
+        fetch("http://25.49.76.159:8060/api/Cadastros/eleicao", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(objeto)  // Envia o objeto convertido para JSON
+        })
+        .then(response => response.json())  // Trata a resposta da API
+        .then(data => {
+            alert("Resposta do servidor: " + JSON.stringify(data));  // Exibe a resposta no alert
+        })
+        .catch(error => {
+          alert("Erro ao enviar os dados:" + error);  // Exibe erros, caso haja
+        });
+    }
+}
