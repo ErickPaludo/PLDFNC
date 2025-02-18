8228540454069222ee3b52818ffe428d77023c5c
Erick <erickpaludoyt@gmail.com>
2025-02-18 08:13:09 -0300

    1.0.5 - Site Cadastro - Convertido de JS puro para React, criação de: *Cadastro de Usuário *Cadastro de Débito e crédito *Tela de login

--- SITE/FNCREACT/financ/src/App.js
+++ SITE/FNCREACT/financ/src/App.js
@@ -22,6 +22,13 @@ function App() {
   const[passLogin,setPassLogin] = useState("")
   const[showPassword,setShowPass] = useState(false)
 
+  const[cadTitulo,setCadTitulo] = useState("")
+  const[cadValor,setCadValor] = useState("")
+  const[cadDescricao,setCadDescricao] = useState("")
+  const[cadData,setCadData] = useState(new Date().toLocaleString())
+  const[cadCategoria,setCadCategoria] = useState("")
+  const[cadParcela,setCadParcela] = useState(1);
+
   async function handleSearch(){
     if(firstName === '' || lastName === '' || email === '' || user === '' || password === '' || confPassword === ''  ){
       Swal.fire({
@@ -171,29 +178,36 @@ function App() {
       <div>
       <div className="containerInput">
       <p>Título: </p>
-      <input type="text" placeholder="titulo" onChange={(e) => setUserLogin(e.target.value)} ></input>
+      <input type="text" placeholder="titulo" onChange={(e) => setCadTitulo(e.target.value)} ></input>
       </div>
       <div className="containerInput">
       <p>Valor: </p>
-      <input type="number" placeholder="R$ 00,00" value={userLogin} onChange={(e) => setUserLogin(e.target.value)} ></input>
+      <input type="number" placeholder="R$ 00,00" onChange={(e) => setCadValor(e.target.value)} ></input>
       </div>
       <div className="containerInput">
       <p>Data: </p>
-        <input type="datetime-local"></input>
+        <input type="date"></input>
       </div>
       <div className="containerInput">
       <p>Descrição: </p>
-      <input type="text" value={userLogin} onChange={(e) => setUserLogin(e.target.value)} ></input>
+      <input type="text" onChange={(e) => setCadDescricao(e.target.value)} ></input>
       </div>
       <div className="containerInput">
       <p>Gasto: </p>
-      <select id="categoria" name="categoria" required>
-                    <option value="" disabled selected>Selecione uma categoria</option>
-                    <option value="debito">Débito</option>  
-                    <option value="credito">Crédito</option>         
-                </select>
+      <select id="categoria" name="categoria" value={cadCategoria} onChange={(e) => setCadCategoria(Number(e.target.value))} required>
+      <option value="" disabled>Selecione uma categoria</option>
+      <option value={0}>Débito</option>  
+      <option value={1}>Crédito</option>         
+</select>
+
       </div>
-      
+       {cadCategoria === 1 && (
+      <div className="containerInput">
+      <p>Parcelas: </p>
+      <input type="number" value={cadParcela} onChange={(e) => setCadParcela(e.target.value)} ></input>
+      </div>
+      )}
+
       <div className="containerInput">  
       <button className="buttonEnv" >
         Entrar
